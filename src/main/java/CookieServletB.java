79d33f6b48beefa6b9cb7eff8ee02bd00bdb0035
dayakarap06-design <dayakarap06@gmail.com>
2026-04-22 22:01:09 -0700

    lab_8b

---
+++ src/main/java/CookieServletB.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/CookieServletB")
+public class CookieServletB extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("username");
+        int count = 1;
+
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+
+                if (c.getName().equals("username")) {
+                    name = c.getValue();
+                }
+
+                if (c.getName().equals("visitCount")) {
+                    count = Integer.parseInt(c.getValue());
+                    count++;
+                }
+            }
+        }
+
+        Cookie nameCookie = new Cookie("username", name);
+        Cookie countCookie = new Cookie("visitCount", String.valueOf(count));
+
+        nameCookie.setMaxAge(60);
+        countCookie.setMaxAge(60);
+
+        response.addCookie(nameCookie);
+        response.addCookie(countCookie);
+
+        out.println("<html><body>");
+        out.println("<h2>Welcome back " + name + "!</h2>");
+        out.println("<h3>You have visited this page " + count + " times.</h3>");
+
+        out.println("<h3>Cookie Details:</h3>");
+
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                out.println("<p>");
+                out.println("Name: " + c.getName() + "<br>");
+                out.println("Value: " + c.getValue() + "<br>");
+                out.println("Max Age: " + c.getMaxAge());
+                out.println("</p><hr>");
+            }
+        }
+
+        out.println("<p><b>Note:</b> Cookies expire in 60 seconds.</p>");
+        out.println("</body></html>");
+    }
+}
